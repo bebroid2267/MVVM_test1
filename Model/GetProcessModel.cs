0dffec5c88230e670a24e9617e09ada39577c847
Les go baby <porkatube@gmail.com>
2024-03-27 13:01:04 +0500

    note get process

---
+++ Model/GetProcessModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_test1.Model
+{
+    internal class GetProcessModel : BindableBase
+    {
+        public GetProcessModel() 
+        {
+            WorksProcess = _WorkProcess;
+
+        }
+        public ObservableCollection<ProcessTime> _WorkProcess
+        {
+            get { return WorksProcess; }
+            set
+            {
+                Application.Current.Dispatcher.InvokeAsync(() =>
+                {
+                    WorksProcess = value;
+                    OnPropertyChanged(nameof(WorksProcess));
+                });
+            }
+        }
+
+        public G
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        private ObservableCollection<ProcessTime> WorksProcess;
+        
+    }
+}
